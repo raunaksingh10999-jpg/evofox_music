189e3dd13e6ba51aca22750314f8df68b456fae8
raunaksingh10999-jpg <raunaksingh10999@gmail.com>
2026-04-04 11:26:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-
+API_ID = 123456              # from my.telegram.org
+API_HASH = "your_api_hash"   # from my.telegram.org
+STRING_SESSION = "your_string_session"
+BOT_TOKEN = "your_bot_token" # from @BotFather (optional, for commands)
