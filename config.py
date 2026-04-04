5824d24895731be2a5c4e43fc2659bd8d35f56cd
Raunak Singh <raunaksingh10999@gmail.com>
2026-04-04 18:50:19 +0530

    Update config.py with requested keys

--- config.py
+++ config.py
@@ -2,3 +2,16 @@ API_ID = 123456              # from my.telegram.org
 API_HASH = "your_api_hash"   # from my.telegram.org
 STRING_SESSION = "your_string_session"
 BOT_TOKEN = "your_bot_token" # from @BotFather (optional, for commands)
+
+# --- Bot Owner & Admin Configuration ---
+OWNER_ID = 123456789         # Your Telegram User ID
+OWNER_USERNAME = "your_username" # Your Telegram Username (without @)
+SUDO_USERS = []              # List of Sudo User IDs, e.g. [123456, 987654]
+BOT_USERNAME = "yourbot"     # Your Bot Username (without @)
+
+# --- Database & Logging ---
+MONGO_DB_URL = "mongodb+srv://..."  # MongoDB Connection URL
+LOG_GROUP_ID = -100123456789        # ID of the Log Group
+
+# --- Customization ---
+START_IMAGE = "https://telegra.ph/file/your-music-image.jpg" # Image on /start
